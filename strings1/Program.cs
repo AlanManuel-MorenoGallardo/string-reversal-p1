49a4f1b61090818b879934601a617ee3b552864c
Alan Manuel Moreno Gallardo <41549805+AlanManuel-MorenoGallardo@users.noreply.github.com>
2018-07-22 14:59:19 -0400

    created project
    pushing initial files

---
+++ strings1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace strings1
+{
+    class Program
+    {
+        public void mainRoutine()
+        {
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            mainRoutine();
+        }
+    }
+}
