45bfe6ce6539b62d6cf2ea72c6c60c029161b21e
Alan Manuel Moreno Gallardo <41549805+AlanManuel-MorenoGallardo@users.noreply.github.com>
2018-07-22 22:43:48 -0400

    added string reverser
    reverses strings
    renamed Main() program...

---
+++ strings1/MainProg.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace strings1
+{
+    class MainProg
+    {
+
+
+        static void Main(string[] args)
+        {
+            ReverseString rs = new ReverseString();
+
+            Console.WriteLine("Instantiated projects.");
+            bool continuing = true;
+            
+            while (continuing)
+            {
+                Console.WriteLine("Select: 1. String Reverser");
+                int selection = Convert.ToInt32(Console.ReadLine());
+                if(selection == 1)
+                {
+                    Console.WriteLine("Input string to reverse:");
+                    string input = Console.ReadLine();
+                    Console.WriteLine("Result: " + rs.reverse(input));
+                }
+                else
+                {
+                    Console.WriteLine("Invalid option");
+                }
+                Console.WriteLine("Do you wish to continue?");
+                Console.WriteLine("1 to continue, 2 to stop.");
+                int select2 = Convert.ToInt32(Console.ReadLine());
+                if(select2 != 1)
+                {
+                    continuing = false;
+                }
+            }
+
+        }
+
+    }
+}
---
+++ strings1/ReverseString.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace strings1
+{
+    class ReverseString
+    {
+        public string reverse(string input)
+        {
+            int strLength = input.Length - 1;
+            List<string> stringtoreverse = new List<string>();
+            while (strLength >= 0)
+            {
+                stringtoreverse.Add(input.Substring(strLength - 1, strLength));
+                strLength--;
+            }
+            string final = string.Join(",", stringtoreverse.ToArray());
+
+            return final;
+        }
+
+
+
+
+
+    }
+}
